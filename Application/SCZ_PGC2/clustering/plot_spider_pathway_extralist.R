3f34b5901865a9085af94fae6a7b581d318253f8
luciat <lucia_trastulla@psych.mpg.de>
2023-03-09 12:31:24 +0100

    update

--- Application/SCZ_PGC2/clustering/plot_spider_pathway_extralist.R
+++ Application/SCZ_PGC2/clustering/plot_spider_pathway_extralist.R
@@ -31,23 +31,25 @@ pval_corr_thr <- 0.05
 
 pathway_feat_wiki <- get(load(sprintf('%smatchUKBB_customPath_WikiPath2019HumanOriginal_corrPCs_tscoreClusterCases_featAssociation.RData', fold_cl)))
 pathway_feat_CMCset <- get(load(sprintf('%smatchUKBB_customPath_CMC_GeneSetsOriginal_corrPCs_tscoreClusterCases_featAssociation.RData', fold_cl)))
+pathway_feat_tot <- get(load(sprintf('%smatchUKBB_pathOriginal_filtJS0.2_corrPCs_tscoreClusterCases_featAssociation.RData', fold_cl)))
 
 
-extra_pathway_list <- data.frame(path = c("Cytokines and Inflammatory Response WP530",
-                        "Complement Activation WP545",
-                        "Insulin Signaling WP481",
-                        "MO:PresynapticCompartmentProteins_Morciano",
-                        "Neuronal proteome:PSD",
-                        "MouseMeta:green_M5_Mitochondria",
-                        "CTX:green_M10_GlutamatergicSynapticFunction",
-                        "MO:Endoplasmic_Reticulum_Foster",
-                        "MO:Plasma_membrane_Foster",
-                        "Oxidative Damage WP3941",
-                        "Type II interferon signaling (IFNG) WP619"), 
-                        tissue = c("Brain_Cerebellum", "Brain_Cerebellar_Hemisphere", 
-                                   "DLPC_CMC", "DLPC_CMC", "DLPC_CMC", "DLPC_CMC", "DLPC_CMC",
-                                   "DLPC_CMC", "DLPC_CMC", "Cells_EBV-transformed_lymphocytes", 
-                                   "Cells_EBV-transformed_lymphocytes"))
+extra_pathway_list <- data.frame(path = c("positive regulation of T cell differentiation",
+                                          "Cytokines and Inflammatory Response WP530",
+                                          "Complement Activation WP545",
+                                          "Insulin Signaling WP481",
+                                          "MO:PresynapticCompartmentProteins_Morciano",
+                                          "Neuronal proteome:PSD",
+                                          "MouseMeta:green_M5_Mitochondria",
+                                          "CTX:green_M10_GlutamatergicSynapticFunction",
+                                          "MO:Endoplasmic_Reticulum_Foster",
+                                          "MO:Plasma_membrane_Foster",
+                                          "Oxidative Damage WP3941",
+                                          "Type II interferon signaling (IFNG) WP619"), 
+                                 tissue = c("DLPC_CMC","Brain_Cerebellum", "Brain_Cerebellar_Hemisphere", 
+                                            "DLPC_CMC", "DLPC_CMC", "DLPC_CMC", "DLPC_CMC", "DLPC_CMC",
+                                            "DLPC_CMC", "DLPC_CMC", "Cells_EBV-transformed_lymphocytes", 
+                                            "Cells_EBV-transformed_lymphocytes"))
 
 
 # spider plot for a selection of pathways
@@ -76,7 +78,7 @@ get_pathway_scores <- function(pathway_feat, keep_pathways, res_cl) {
   
   new_id <- paste(keep_pathways$path, keep_pathways$tissue, sep = "_")
   
-  keep_pathways_tissues <- lapply(pathway_feat$test_feat, function(x) 
+  keep_pathways_tissues <- lapply(pathway_feat$test_feat, function(x)
     x[paste(x$feat, x$tissue, sep = "_") %in% new_id & x$pval_corr <= 0.05, ]) %>% 
     dplyr::bind_rows() %>%
     dplyr::group_by(comp, feat) %>%
@@ -100,9 +102,14 @@ get_pathway_scores <- function(pathway_feat, keep_pathways, res_cl) {
   pathscore <- do.call(cbind, pathscore)
   
   gr_id <- sort(unique(res_cl$gr))
-  df_mean <- t(sapply(gr_id, function(x) colMeans(pathscore[res_cl$gr == x,], na.rm = T)))
+  df_mean <- t(sapply(gr_id, function(x) colMeans(pathscore[res_cl$gr == x, , drop=F], na.rm = T)))
+  if(nrow(keep_pathways_tissues) == 1){
+    df_mean <- t(df_mean)
+    rownames(df_mean) <- NULL
+    colnames(df_mean) <- colnames(pathscore)
+  }
   df_mean <- cbind(data.frame(group = paste0('gr_',gr_id)), df_mean)
-  df_mean[, -1] <- apply(df_mean[,-1], 2, rescale)
+  df_mean[, -1] <- apply(df_mean[,-1, drop = F], 2, rescale)
   
   return(list(mean = df_mean, pathways = keep_pathways_tissues))
 }
@@ -115,8 +122,9 @@ library(ggradar)
 res_cl <- pathway_feat_wiki$cl                                  
 input_spider_1 <- get_pathway_scores(pathway_feat_wiki, extra_pathway_list, res_cl)
 input_spider_2 <- get_pathway_scores(pathway_feat_CMCset, extra_pathway_list, res_cl)
-input_spider <- cbind(input_spider_1$mean, input_spider_2$mean[,-1])
-keep_pathways_tissues <- c(input_spider_1$pathways, input_spider_2$pathways)
+input_spider_3 <- get_pathway_scores(pathway_feat_tot, extra_pathway_list, res_cl)
+input_spider <- cbind(input_spider_1$mean, input_spider_2$mean[,-1], input_spider_3$mean[,-1, drop = F])
+keep_pathways_tissues <- c(input_spider_1$pathways, input_spider_2$pathways, input_spider_3$pathways)
 input_spider
 
 df_mean <- input_spider
