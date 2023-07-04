cbd76eca0e3be5880d9551c0bd680c0ad55bf6bc
luciat <lucia_trastulla@psych.mpg.de>
2023-07-04 16:29:16 +0200

    add part2 PGC GWAS

---
+++ Application/PriLer/GTEx_v6p/ElNet_withPrior_part2_200kb_tissue_PGCgwas1e-2_smallE.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+t=$1
+
+priorInd=$(awk '{print $1}' /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/OUTPUT_SCRIPTS_v2/${t}/priorName_PGCgwas_withIndex.txt)
+f=/psycl/g/mpsziller/lucia/
+git_fold=${f}castom-igex/Software/model_training/
+
+${git_fold}PriLer_part2_run.R \
+    --covDat_file /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/INPUT_DATA/Covariates/${t}/covariates_EuropeanSamples.txt \
+    --genoDat_file /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/INPUT_DATA/Genotype_data/Genotype_dosage_ \
+    --geneExp_file /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/INPUT_DATA/RNAseq_data/${t}/RNAseq_filt.txt \
+    --ncores 15 \
+    --outFold /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/OUTPUT_SCRIPTS_v2/${t}/200kb/PGC_GWAS_bin1e-2/ \
+    --InfoFold /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/OUTPUT_SCRIPTS_v2/${t}/ \
+    --functR ${git_fold}PriLer_functions.R \
+    --part1Res_fold /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/OUTPUT_SCRIPTS_v2/${t}/200kb/ \
+    --priorDat_file /psycl/g/mpsziller/lucia/PriLer_PROJECT_GTEx/OUTPUT_SCRIPTS_v2/priorMatrix_ \
+    --priorInf ${priorInd[@]} \
+    --E_set 1 1.5 2 2.5 3 4 5 6 7 8 9 10 11 12 15
+
+
