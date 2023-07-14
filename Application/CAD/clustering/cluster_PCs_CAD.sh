b53f7f2aa539ea0bf31eb127286a5a74377be130
luciat <lucia_trastulla@psych.mpg.de>
2023-07-14 22:23:55 +0200

    update PCs cluster

--- Application/CAD/clustering/cluster_PCs_CAD.sh
+++ Application/CAD/clustering/cluster_PCs_CAD.sh
@@ -5,12 +5,12 @@
 #SBATCH --nodes=1
 #SBATCH --mem=30G
 
-module load R/3.5.3
+module load r_anaconda/4.0.3
+R_LIBS_USER=/u/luciat/R/x86_64-pc-linux-gnu-library/4.0/
 
 cd /psycl/g/mpsziller/lucia/CAD_UKBB/eQTL_PROJECT/
 git_fold=/psycl/g/mpsziller/lucia/castom-igex/Software/model_clustering/
 
-fold=OUTPUT_GTEx/predict_CAD/${t}/200kb/CAD_GWAS_bin5e-2/UKBB/devgeno0.01_testdevgeno0/
 fold=INPUT_DATA_GTEx/CAD/Covariates/UKBB/CAD_HARD_clustering/
 
 ${git_fold}cluster_PGmethod_PCs_run.R \
@@ -19,5 +19,5 @@ ${git_fold}cluster_PGmethod_PCs_run.R \
 	--type_cluster $1 \
 	--outFold ${fold} \
 	--functR ${git_fold}clustering_functions.R \
-	--kNN_par 30
+	--kNN_par 20
 
