5b9c03e8658c0a6039ff1b4f5b190973f0ecd7c8
Lucia Trastulla <luciat@int6.local.snellius.surf.nl>
2023-08-10 16:39:58 +0200

    mod outliers detection

--- Application/SCZ_PGC2/clustering/outliersDet_tscore_corrPCs_Cases_CMC_matchUKBB.sh
+++ Application/SCZ_PGC2/clustering/outliersDet_tscore_corrPCs_Cases_CMC_matchUKBB.sh
@@ -4,52 +4,46 @@
 #SBATCH -N 1
 #SBATCH -t 24:00:00
 #SBATCH --mem=40G
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
 
 
-module load 2019
-module load R/3.5.1-intel-2019b
+module load 2022
+module load R/4.2.1-foss-2022a
+git_fold=${HOME}/castom-igex/Software/model_clustering/
 
+# copy TWAS res
+cd ${HOME}/eQTL_PROJECT
+mkdir -p OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
+cp OUTPUT_all/Meta_Analysis_SCZ/DLPC_CMC/pval_Dx_pheno_covCorr.RData ${TMPDIR}/
+readarray -t name_cohorts < INPUT_DATA/SCZ_cohort_names_CLUST
 
-cd /home/luciat/eQTL_PROJECT/
-
-readarray -t cohorts < /home/luciat/eQTL_PROJECT/INPUT_DATA/SCZ_cohort_names_CLUST
-name_cohorts=${cohorts[@]}
-
+s_sh=/scratch-shared/luciat/
 input_file=()
 cov_file=()
-git_fold=/home/luciat/castom-igex/Software/model_clustering/
-
-mkdir -p ${TMPDIR}/tmp_CMC_t
-cp OUTPUT_all/Meta_Analysis_SCZ/DLPC_CMC/pval_Dx_pheno_covCorr.RData ${TMPDIR}/tmp_CMC_t/
 
 for c in ${name_cohorts[@]}
 do
-	echo ${c}
-   	mkdir -p ${TMPDIR}/tmp_CMC_t/${c}
-	cp OUTPUT_CMC/predict_PGC/200kb/${c}/devgeno0.01_testdevgeno0/predictedTscores.txt ${TMPDIR}/tmp_CMC_t/${c}/
-	input_file+=(${TMPDIR}/tmp_CMC_t/${c}/predictedTscores.txt)
+	input_file+=(${s_sh}/OUTPUT_CMC/predict_PGC/200kb/${c}/devgeno0.01_testdevgeno0/predictedTscores.txt)
 	cov_file+=(INPUT_DATA/Covariates/${c}.covariateMatrix_old.txt)
 done
 
-thr=$1
 ${git_fold}detect_outliers_corrPCs_multipleCohorts_run.R \
 	--tissues_name DLPC_CMC \
 	--inputFile ${input_file[@]} \
 	--name_cohorts ${name_cohorts[@]} \
 	--type_cluster Cases \
-	--pvalresFile ${TMPDIR}/tmp_CMC_t/pval_Dx_pheno_covCorr.RData \
+	--pvalresFile ${TMPDIR}/pval_Dx_pheno_covCorr.RData \
 	--sampleAnnFile ${cov_file[@]} \
 	--pval_id 1 \
 	--type_data tscore \
 	--corr_thr 0.9 \
 	--type_input zscaled \
 	--tissues_name DLPC_CMC \
-	--outFold ${TMPDIR}/tmp_CMC_t/matchUKBB_ \
+	--outFold ${TMPDIR}/matchUKBB_ \
 	--functR ${git_fold}clustering_functions.R \
 	--min_genes_path 2 \
 	--genes_to_filter compare_prediction_UKBB_SCZ-PGC/DLPC_CMC_filter_genes_matched_datasets.txt
 
-cp ${TMPDIR}/tmp_CMC_t/matchUKBB*cluster* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
-
-rm -r ${TMPDIR}/tmp_CMC_t/
+cp ${TMPDIR}/matchUKBB*cluster* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
 
