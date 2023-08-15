faf8c225dd51c351c8df1487afab2979261624d9
Lucia Trastulla <luciat@int5.local.snellius.surf.nl>
2023-08-15 11:23:52 +0200

    update cluster PCs

--- Application/SCZ_PGC2/clustering/PriLer_cluster_PCs_matchUKBB.sh
+++ Application/SCZ_PGC2/clustering/PriLer_cluster_PCs_matchUKBB.sh
@@ -2,29 +2,23 @@
 #SBATCH -o /home/luciat/eQTL_PROJECT/err_out_fold/cluster_PCs_%x_matchUKBB.out
 #SBATCH -e /home/luciat/eQTL_PROJECT/err_out_fold/cluster_PCs_%x_matchUKBB.err
 #SBATCH -N 1
-#SBATCH --mem=90G
+#SBATCH --mem=28G
 #SBATCH -t 24:00:00
-#SBATCH -p fat
 
 
-module load 2019
-module load R/3.5.1-intel-2019b
+module load 2022
+module load R/4.2.1-foss-2022a
+git_fold=${HOME}/castom-igex/Software/model_clustering/
 
-cd /home/luciat/eQTL_PROJECT/
-
-git_fold=/home/luciat/castom-igex/Software/model_clustering/
-
-mkdir -p ${TMPDIR}/tmp_PCs/
+cd ${HOME}/eQTL_PROJECT/
 
 ${git_fold}cluster_PGmethod_PCs_run.R \
 	--PCs_input_file INPUT_DATA/Covariates/PCs_cluster/C1-20_PGC_clustering.RData \
 	--type_cluster Cases \
 	--sampleAnnFile INPUT_DATA/Covariates/PCs_cluster/samples_PCs_clustering.txt \
-	--outFold ${TMPDIR}/tmp_PCs/matchUKBB_ \
+	--outFold ${TMPDIR}/matchUKBB_ \
 	--functR ${git_fold}clustering_functions.R \
-	--sampleOutFile OUTPUT_all/matchUKBB_samples_to_remove_outliersUMAP_tscore_corrPCs_zscaled_clusterCases.txt
+	--sampleOutFile OUTPUT_all/clustering_res_matchUKBB_corrPCs/matchUKBB_samples_to_remove_outliersUMAP_tscore_corrPCs_zscaled_clusterCases.txt
 
-cp ${TMPDIR}/tmp_PCs/matchUKBB_*cluster* INPUT_DATA/Covariates/PCs_cluster/
-
-rm -r ${TMPDIR}/tmp_PCs/
+cp ${TMPDIR}/matchUKBB_*cluster* INPUT_DATA/Covariates/PCs_cluster/
 
