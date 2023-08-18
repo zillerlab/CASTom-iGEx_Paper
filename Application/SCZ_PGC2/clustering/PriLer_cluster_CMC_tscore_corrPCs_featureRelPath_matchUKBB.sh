00e52117ec04cb491fa813e3d0e2fda4deafe703
Lucia Trastulla <luciat@int6.local.snellius.surf.nl>
2023-08-18 17:41:36 +0200

    update scripts for features association

--- Application/SCZ_PGC2/clustering/PriLer_cluster_CMC_tscore_corrPCs_featureRelPath_matchUKBB.sh
+++ Application/SCZ_PGC2/clustering/PriLer_cluster_CMC_tscore_corrPCs_featureRelPath_matchUKBB.sh
@@ -2,33 +2,32 @@
 #SBATCH -o /home/luciat/eQTL_PROJECT/err_out_fold/cluster_tscore_corrPCs_zscaled_CMC_%x_featRelPath.out
 #SBATCH -e /home/luciat/eQTL_PROJECT/err_out_fold/cluster_tscore_corrPCs_zscaled_CMC_%x_featRelPath.err
 #SBATCH -N 1
-#SBATCH --mem=100G
-#SBATCH --tasks-per-node 10
-#SBATCH -t 24:00:00
-#SBATCH -p fat
+#SBATCH --mem=112G
+#SBATCH --tasks-per-node 16
+#SBATCH -t 30:00:00
 
-module load 2019
-module load R/3.5.1-intel-2019b
+module load 2022
+module load R/4.2.1-foss-2022a
+git_fold=${HOME}/castom-igex/Software/model_clustering/
+s_sh=/scratch-shared/luciat/
 
-cd /home/luciat/eQTL_PROJECT/
-git_fold=/home/luciat/castom-igex/Software/model_clustering/
+cd ${HOME}/eQTL_PROJECT/
 
-readarray -t name_cohorts < /home/luciat/eQTL_PROJECT/INPUT_DATA/SCZ_cohort_names_CLUST
+readarray -t name_cohorts < INPUT_DATA/SCZ_cohort_names_CLUST
 readarray -t tissues_gtex < OUTPUT_GTEx/Tissue_PGCgwas_red
 
-input_fold_t=(OUTPUT_CMC/predict_PGC/200kb/)
+input_fold_t=(${s_sh}/OUTPUT_CMC/predict_PGC/200kb/)
 cov_file=()
 tissues_tot=(DLPC_CMC)
 pval_file=(OUTPUT_all/Meta_Analysis_SCZ/DLPC_CMC/pval_Dx_pheno_covCorr.RData)
 path_ann=(OUTPUT_all/Meta_Analysis_SCZ/DLPC_CMC/selected_pathways_JSthr0.2.txt)
 
-mkdir -p ${TMPDIR}/tmp_CMC_t
-cp OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric_minimal.RData ${TMPDIR}/tmp_CMC_t/
+cp OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric_minimal.RData ${TMPDIR}/
 
 for t in ${tissues_gtex[@]}
 do
 	echo ${t}
-	input_fold_t+=(OUTPUT_GTEx/predict_PGC/${t}/200kb/PGC_GWAS_bin1e-2/)
+	input_fold_t+=(${s_sh}OUTPUT_GTEx/predict_PGC/${t}/200kb/PGC_GWAS_bin1e-2/)
 	pval_file+=(OUTPUT_all/Meta_Analysis_SCZ/${t}/pval_Dx_pheno_covCorr.RData)
 	path_ann+=(OUTPUT_all/Meta_Analysis_SCZ/${t}/selected_pathways_JSthr0.2.txt)
 	tissues_tot+=(${t})
@@ -55,16 +54,13 @@ ${git_fold}cluster_associatePath_corrPCs_multipleCohorts_run.R \
 	--pval_id 1 \
 	--type_data path \
 	--type_input zscaled  \
-	--outFold ${TMPDIR}/tmp_CMC_t/matchUKBB_ \
+	--outFold ${TMPDIR}/matchUKBB_ \
 	--functR ${git_fold}clustering_functions.R \
-	--clusterFile ${TMPDIR}/tmp_CMC_t/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric_minimal.RData \
+	--clusterFile ${TMPDIR}/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric_minimal.RData \
 	--ncores 10 \
 	--thr_js 0.2 \
 	--path_filt_file ${path_ann[@]}
 
-cp ${TMPDIR}/tmp_CMC_t/matchUKBB_pathOriginal*Cluster* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
-
-rm -r ${TMPDIR}/tmp_CMC_t/
-
+cp ${TMPDIR}/matchUKBB_pathOriginal*Cluster* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
 
 
--- Application/SCZ_PGC2/clustering/PriLer_cluster_CMC_tscore_corrPCs_featureRelTscore_matchUKBB.sh
+++ Application/SCZ_PGC2/clustering/PriLer_cluster_CMC_tscore_corrPCs_featureRelTscore_matchUKBB.sh
@@ -2,34 +2,32 @@
 #SBATCH -o /home/luciat/eQTL_PROJECT/err_out_fold/cluster_tscore_corrPCs_zscaled_CMC_%x_featRelTscore.out
 #SBATCH -e /home/luciat/eQTL_PROJECT/err_out_fold/cluster_tscore_corrPCs_zscaled_CMC_%x_featRelTscore.err
 #SBATCH -N 1
-#SBATCH --mem=150G
-#SBATCH --tasks-per-node 10
-#SBATCH -t 24:00:00
-#SBATCH -p fat
+#SBATCH --mem=112G
+#SBATCH --tasks-per-node 16
+#SBATCH -t 30:00:00
 
-module load 2019
-module load R/3.5.1-intel-2019b
+module load 2022
+module load R/4.2.1-foss-2022a
+git_fold=${HOME}/castom-igex/Software/model_clustering/
+s_sh=/scratch-shared/luciat/
 
-cd /home/luciat/eQTL_PROJECT/
-git_fold=/home/luciat/castom-igex/Software/model_clustering/
+cd ${HOME}/eQTL_PROJECT/
 
-readarray -t name_cohorts < /home/luciat/eQTL_PROJECT/INPUT_DATA/SCZ_cohort_names_CLUST
+readarray -t name_cohorts < INPUT_DATA/SCZ_cohort_names_CLUST
 readarray -t tissues_gtex < OUTPUT_GTEx/Tissue_PGCgwas_red
 
-input_fold_t=(OUTPUT_CMC/predict_PGC/200kb/)
+input_fold_t=(${s_sh}OUTPUT_CMC/predict_PGC/200kb/)
 cov_file=()
 tissues_tot=(DLPC_CMC)
 gene_info=(OUTPUT_CMC/train_CMC/200kb/resPrior_regEval_allchr.txt)
 pval_file=(OUTPUT_all/Meta_Analysis_SCZ/DLPC_CMC/pval_Dx_pheno_covCorr.RData)
 
-
-mkdir -p ${TMPDIR}/tmp_CMC_t
-cp OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric.RData ${TMPDIR}/tmp_CMC_t/
+cp OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric.RData ${TMPDIR}
 
 for t in ${tissues_gtex[@]}
 do
 	echo ${t}
-	input_fold_t+=(OUTPUT_GTEx/predict_PGC/${t}/200kb/PGC_GWAS_bin1e-2/)
+	input_fold_t+=(${s_sh}OUTPUT_GTEx/predict_PGC/${t}/200kb/PGC_GWAS_bin1e-2/)
 	pval_file+=(OUTPUT_all/Meta_Analysis_SCZ/${t}/pval_Dx_pheno_covCorr.RData)
 	tissues_tot+=(${t})
 	gene_info+=(OUTPUT_GTEx/train_GTEx/${t}/200kb/PGC_GWAS_bin1e-2/resPrior_regEval_allchr.txt)
@@ -55,18 +53,17 @@ ${git_fold}cluster_associateFeat_corrPCs_multipleCohorts_run.R \
 	--pval_id 1 \
 	--type_data tscore \
 	--type_input zscaled  \
-	--outFold ${TMPDIR}/tmp_CMC_t/matchUKBB_ \
+	--outFold ${TMPDIR}/matchUKBB_ \
 	--functR ${git_fold}clustering_functions.R \
-	--clusterFile ${TMPDIR}/tmp_CMC_t/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric.RData \
+	--clusterFile ${TMPDIR}/matchUKBB_tscore_corrPCs_zscaled_clusterCases_PGmethod_HKmetric.RData \
 	--ncores 10 \
 	--geneInfoFile ${gene_info[@]} \
 	--pvalcorr_thr 0.01 
 
-cp ${TMPDIR}/tmp_CMC_t/matchUKBB_tscoreOriginal*Cluster* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
+cp ${TMPDIR}/matchUKBB_tscoreOriginal*Cluster* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
 
-cp ${TMPDIR}/tmp_CMC_t/matchUKBB_*summary* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
+cp ${TMPDIR}/matchUKBB_*summary* OUTPUT_CMC/predict_PGC/200kb/Meta_Analysis_SCZ/devgeno0.01_testdevgeno0/update_corrPCs/
 
-rm -r ${TMPDIR}/tmp_CMC_t/
 
 
 
